feac69c1ce75f1862fecc6a8c5f68148f241dd3e
arshavir azarfar <altair.cena@gmail.com>
2020-08-18 03:05:40 +0430

    Updates

---
+++ precache-manifest.bc06376c561f01fffe9a207c231f744a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a32da7fe2bc03558452ac31d783b08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "556ce1b0ed402951b3cc",
+    "url": "/static/css/2.e15a0812.chunk.css"
+  },
+  {
+    "revision": "556ce1b0ed402951b3cc",
+    "url": "/static/js/2.429cbfe5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.429cbfe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a3583446ff126c984b",
+    "url": "/static/js/main.963d408b.chunk.js"
+  },
+  {
+    "revision": "c84c9bd6968fc1ad6fed",
+    "url": "/static/js/runtime-main.366cc330.js"
+  }
+]);
